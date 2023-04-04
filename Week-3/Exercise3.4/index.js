8f74bf48e4edea17c98f91f425b819c41b7d3418
abhinav <singh.abhinav6996@gmail.com>
2023-04-04 12:52:04 +0530

    chore: refactor logic for stack implementation

--- Week-3/Exercise3.4/index.js
+++ Week-3/Exercise3.4/index.js
@@ -6,7 +6,7 @@ function createStack() {
     },
     pop() {
       return this.items.pop();
-    }
+    },
   };
 }
 
@@ -14,32 +14,43 @@ const stack = createStack();
 
 stack.push(10);
 stack.push(5);
-stack.pop();// => 5
-stack.items;// => [10]
-stack.items = [10, 100, 1000];// Encapsulation broken!
+stack.pop(); // => 5
+stack.items; // => [10]
+stack.items = [10, 100, 1000]; // Encapsulation broken!
 
 // In the above example stack.items is resulting 10 because we are accessing items array directly and thus we can read its value
 
-
 //new implementation
-function createStack() {
-  const items = [];
+// function createStack() {
+//   const items = [];
 
+//   return {
+//     push(item) {
+//       items.push(item);
+//     },
+//     pop() {
+//       return items.pop()
+//     }
+//   }
+// }
+
+function createStack() {
   return {
-    push(item) {
-      items.push(item);
+    items: [],
+    push: function (item) {
+      this.items.push(item);
     },
-    pop() {
-      return items.pop()
-    }
-  }
+    pop: function () {
+      return this.items.pop();
+    },
+  };
 }
 
 const stack = createStack();
 
 stack.push(10);
 stack.push(5);
-stack.pop();// => 5
-stack.items;// => undefined
+stack.pop(); // => 5
+stack.items; // => undefined
 
 // In this code stack.items is returning undefined because items is not directly accessible from outside the function as items array is within createStack closure  and providing only push and pop to change value through indirect access.
